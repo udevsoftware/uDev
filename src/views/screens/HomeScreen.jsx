0181203b59c0f12cf741ec9d7283bab8d7a55f3a
elianfarias <elian.farias@axiomait.com>
2023-03-02 11:35:45 -0300

    add logo in whatsapp sticky button

--- src/views/screens/HomeScreen.jsx
+++ src/views/screens/HomeScreen.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import Logo from '../../assets/logo.png';
 
 export const HomeScreen = () => {
     return (<>
         Hola
         <FloatingWhatsApp
-        phoneNumber="123456789"
-        accountName="Foo"
+        phoneNumber="1140374130"
+        accountName="uDev Solutions"
         allowEsc
         allowClickAway
         notification
         notificationSound
+        avatar={Logo}
+        chatMessage="¡Para mas información comunicate con nosotros por whatsapp!"
+        placeholder="Ingrese su consulta"
+        statusMessage=""
       />
     </>)
 }
